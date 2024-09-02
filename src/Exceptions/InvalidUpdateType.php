f28cb0f284cbcf2ab6e05443dc44773541d546fa
ThevenRexOff <thevenrexoficial@outlook.es>
2024-09-01 19:33:19 -0500

    Add: InvalidUpdateType Exception

---
+++ src/Exceptions/InvalidUpdateType.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace LaraGram\Exceptions;
+
+use Throwable;
+use UnexpectedValueException;
+
+final class InvalidUpdateType extends UnexpectedValueException implements Throwable
+{
+  
+}
